c116a6e653c115bd1487d7d7730ea815a2746d2b
Terence Minerbrook <terence.minerbrook@gmail.com>
2023-02-23 14:11:27 -0500

    part1:events and state rough complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import { useState } from "react";
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>The app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const Display = ({ counter }) => {
+  return <div>{counter}</div>;
+};
 function App() {
   const [counter, setCounter] = useState(0);
   const increaseByOne = () => setCounter(counter + 1);
@@ -13,31 +27,35 @@ function App() {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
   const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
 
   const handleLeftClick = () => {
-    setClicks({
-      left: clicks.left + 1,
-      ...clicks,
-    });
+    setAll(allClicks.concat("L"));
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    setTotal(updatedLeft + right);
   };
 
   const handleRightClick = () => {
-    setClicks({
-      ...clicks,
-      right: clicks.right + 1,
-    });
-  };
-
-  const Button = ({ onClick, text }) => {
-    return <button onClick={onClick}>{text}</button>;
-  };
-
-  const Display = ({ counter }) => {
-    return <div>{counter}</div>;
+    setAll(allClicks.concat("R"));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
   };
 
   return (
     <div>
+      <History allClicks={allClicks} />
+      <br />
+
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
+      <br />
+      <p>Left{allClicks.join(" ")}Right</p>
+      <p>total{total}</p>
+      <br />
       {left}
       <button onClick={() => setLeft(left + 1)}> left</button>
       <button onClick={() => setRight(right + 1)}> right </button>
