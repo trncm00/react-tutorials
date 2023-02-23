dc62e01051e1510f62f557818c1ecfbfb21767e7
Terence Minerbrook <terence.minerbrook@gmail.com>
2023-02-23 11:44:53 -0500

    part1:objects as params

--- src/App.js
+++ src/App.js
@@ -12,21 +12,20 @@ function App() {
 
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
   const handleLeftClick = () => {
-    const newClicks = {
+    setClicks({
       left: clicks.left + 1,
       ...clicks,
-    };
-    setClicks(newClicks);
+    });
   };
 
   const handleRightClick = () => {
-    const newClicks = {
+    setClicks({
       ...clicks,
       right: clicks.right + 1,
-    };
-    setClicks(newClicks);
+    });
   };
 
   const Button = ({ onClick, text }) => {
