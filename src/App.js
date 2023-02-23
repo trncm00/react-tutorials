ded1932fce5822bb466175c0c21e86dd036f49e7
Terence Minerbrook <terence.minerbrook@gmail.com>
2023-02-23 11:34:48 -0500

    part1:event handlers, buttons

--- src/App.js
+++ src/App.js
@@ -5,14 +5,34 @@ function App() {
   const increaseByOne = () => setCounter(counter + 1);
   const decreaseByOne = () => setCounter(counter - 1);
   const setToZero = () => setCounter(0);
-  const [clicks, setClicks] = useState(0);
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0,
+  });
 
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
 
+  const handleLeftClick = () => {
+    const newClicks = {
+      left: clicks.left + 1,
+      ...clicks,
+    };
+    setClicks(newClicks);
+  };
+
+  const handleRightClick = () => {
+    const newClicks = {
+      ...clicks,
+      right: clicks.right + 1,
+    };
+    setClicks(newClicks);
+  };
+
   const Button = ({ onClick, text }) => {
     return <button onClick={onClick}>{text}</button>;
   };
+
   const Display = ({ counter }) => {
     return <div>{counter}</div>;
   };
@@ -20,10 +40,21 @@ function App() {
   return (
     <div>
       {left}
+      <button onClick={() => setLeft(left + 1)}> left</button>
+      <button onClick={() => setRight(right + 1)}> right </button>
+      {right}
+      <br />
+
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
+      <br />
+      {left}
       <button onClick={() => setLeft(left + 1)}>left</button>
       <button onClick={() => setRight(right + 1)}>right</button>
       {right}
-
+      <br />
       <Display counter={counter} />
       <Button onClick={increaseByOne} text="plus" />
       <Button onClick={setToZero} text="zero'd out" />
