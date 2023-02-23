96ec0cd7ecb269cc080af66a307592235ec3ad6f
Terence Minerbrook <terence.minerbrook@gmail.com>
2023-02-23 11:16:10 -0500

    part 1:useState,left and right

--- src/App.js
+++ src/App.js
@@ -5,16 +5,25 @@ function App() {
   const increaseByOne = () => setCounter(counter + 1);
   const decreaseByOne = () => setCounter(counter - 1);
   const setToZero = () => setCounter(0);
+  const [clicks, setClicks] = useState(0);
 
-  const Button = (props) => {
-    return <button onClick={props.onClick}>{props.text}</button>;
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+
+  const Button = ({ onClick, text }) => {
+    return <button onClick={onClick}>{text}</button>;
   };
-  const Display = (props) => {
-    return <div>{props.counter}</div>;
+  const Display = ({ counter }) => {
+    return <div>{counter}</div>;
   };
 
   return (
     <div>
+      {left}
+      <button onClick={() => setLeft(left + 1)}>left</button>
+      <button onClick={() => setRight(right + 1)}>right</button>
+      {right}
+
       <Display counter={counter} />
       <Button onClick={increaseByOne} text="plus" />
       <Button onClick={setToZero} text="zero'd out" />
